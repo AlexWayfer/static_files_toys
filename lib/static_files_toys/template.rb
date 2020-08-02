1b28d3030b9fa639feac71ced1608b233b3decdb
Alexander Popov <alex.wayfer@gmail.com>
2020-08-02 15:53:30 +0300

    Chomp newline from `grep` options

--- lib/static_files_toys/template.rb
+++ lib/static_files_toys/template.rb
@@ -7,7 +7,7 @@ module StaticFilesToys
 	class Template
 		include Toys::Template
 
-		GREP_OPTIONS = <<~STR
+		GREP_OPTIONS = <<~STR.chomp
 			-ir --exclude-dir={\.git,log,node_modules,extra} --color=always
 		STR
 
